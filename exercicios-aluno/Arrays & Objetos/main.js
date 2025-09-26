1b7a1c907a2435ce723a78cdec72b43927fdf56b
Yago1200 <yagolimaalves2000@gmail.com>
2025-09-25 21:15:20 -0300

    25-09
    2

--- exercicios-aluno/Arrays & Objetos/main.js
+++ exercicios-aluno/Arrays & Objetos/main.js
@@ -1,5 +1,5 @@
 let usoDiasDaSemana = [1, 2, 2.4, 5, 1.2, 0.5, 12];
-let DiasDaSemana = [
+let diasDaSemana = [
     'Domingo      ',
     'Segunda-feira',
     'Terça-feira  ',
@@ -11,12 +11,21 @@ let DiasDaSemana = [
 
 const indicadorDeUso = (numeroDeHoras) => {
     if ( numeroDeHoras <= 1 ) {
-        return '🟢'
+        return '🟢';
     } else if ( numeroDeHoras <= 3) {
-        return '🟡'
+        return '🟡';
     } else {
-        return '🔴'
+        return '🔴';
     }
+}
+
+for(let contador = 0; contador <= 6; contador++) {
+    let farol = indicadorDeUso(usoDiasDaSemana[contador]);
+    let dia = diasDaSemana[contador];
+    let horas = usoDiasDaSemana[contador];
+    let mensagem = (horas > 1) ? 'horas de uso' : 'hora de uso'; // Operador ternário
+
+    console.log(`${farol} 📅 ${dia} | ${horas.toFixed(2)} ${mensagem}`);
 }
 
 
